7a23684cf14b769b4d4faaaca4026867cf7f0982
KonstantyFlint <43811569+KonstantyFlint@users.noreply.github.com>
2024-06-10 08:02:51 +0200

    amendments

--- problem.py
+++ problem.py
@@ -38,6 +38,26 @@ def expand(partials: Fl, edges: Fl) -> Tuple[Fl, Fl]:
     return new_partials, update_info
 
 
+def get_best(partials: Fl, index: int):
+    best = None
+    for partial in partials:
+        cur, prev, l = partial
+        if cur != index:
+            continue
+        if best is None or l < best[2]:
+            best = partial
+    return best
+
+
+def collect_output(partials: Fl, end: int):
+    node = get_best(partials, end)
+    output = []
+    while node is not None:
+        output = [node[0]] + output
+        node = get_best(partials, node[1])
+    return output
+
+
 def solve(edges: Fl, start: int, end: int) -> Fl:
     edges = make_edges_both_ways(edges)
     partials = Fl([(start, None, 0)])
@@ -45,6 +65,7 @@ def solve(edges: Fl, start: int, end: int) -> Fl:
     while updated:
         partials, update_info = expand(partials, edges)
         updated = any(update_info)
+    return collect_output(partials, end)
 
 
 problem = read_file("problem.txt", (int, int, int))
